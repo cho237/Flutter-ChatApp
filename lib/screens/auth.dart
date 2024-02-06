4e4a50d7721c0ae2a69e204d4e2bd2116d6d7e56
cho237 <kinslycho237@gmail.com>
2024-02-06 14:21:01 +0100

    adding authentification

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -26,24 +26,27 @@ class _AuthScreenState extends State<AuthScreen> {
       return;
     }
     _form.currentState!.save();
-    if (_isLogin) {
-    } else {
-      try {
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
-        print(userCredentials);
-      } on FirebaseAuthException catch (error) {
-        if (mounted) {
-          if (error.code == 'email-already-in-use') {}
-          ScaffoldMessenger.of(context).clearSnackBars();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(error.message ?? 'Authentifiction failed'),
-            ),
-          );
-        }
+      }
+    } on FirebaseAuthException catch (error) {
+      if (mounted) {
+        if (error.code == 'email-already-in-use') {}
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentifiction failed'),
+          ),
+        );
       }
     }
   }
