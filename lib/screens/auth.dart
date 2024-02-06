94e727bcdc3c9dccbe4fa578888be0ed977633fb
cho237 <kinslycho237@gmail.com>
2024-02-06 13:37:07 +0100

    adding firebase sdk and signup functionality

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -19,11 +22,30 @@ class _AuthScreenState extends State<AuthScreen> {
   void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid) {
-      _form.currentState!.save();
-      print(_enteredEmail);
+    if (!isValid) {
+      return;
     }
-    return;
+    _form.currentState!.save();
+    if (_isLogin) {
+    } else {
+      try {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+        print(userCredentials);
+      } on FirebaseAuthException catch (error) {
+        if (mounted) {
+          if (error.code == 'email-already-in-use') {}
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.message ?? 'Authentifiction failed'),
+            ),
+          );
+        }
+      }
+    }
   }
 
   @override
@@ -42,7 +64,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   left: 20,
                   right: 20,
                 ),
-                width: 200,
+                width: 160,
                 child: Image.asset('assets/images/chat.png'),
               ),
               Card(
